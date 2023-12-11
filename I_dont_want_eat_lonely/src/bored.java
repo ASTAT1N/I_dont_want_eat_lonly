385453db5efa45d1dcc64598acce6525852a5fcb
Ast@tin <4922865@gmail.com>
2023-12-12 00:46:58 +0900

    I will go to home

--- I_dont_want_eat_lonely/src/bored.java
+++ I_dont_want_eat_lonely/src/bored.java
@@ -1,13 +1,86 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.border.Border;
 
 public class Bored extends JPanel {
-	private String title="";
-    
-    public Bored(){
-        setSize(200,250);
-        setBackground(Color.CYAN);
+    // bored info
+    Color bored_backColor = Color.CYAN;
+    private final int bored_size_width = 200;
+    private final int bored_size_height=250;
+    // title info
+    private JLabel title = new JLabel();
+    private final int title_position_x=10;
+    private final int title_position_y=10;
+    private final int title_size_width=180;
+    private final int title_size_height=20;
+    // image info
+    private ImageIcon image = new ImageIcon();
+    private final int image_position_x=10;
+    private final int image_position_y=40;
+    private final int image_size_width = 180;
+    private final int image_size_height =100;
+    // bottom group info
+    private final int bottomGroup_size_width = 40;
+    private final int bottomGroup_size_height = 40;
+    // host info
+
+    //guest info
+
+
+    // extra info
+    private JLabel extras = new JLabel();
+    private final int extras_position_x=155;
+    private final int extras_position_y=200;
+    public Bored(){ // for text
+        this("AAAA");
+    }
+    public Bored(String _title){
+        // Bored set
+        setLayout(null);
+        setPreferredSize(new Dimension(bored_size_width,bored_size_height));
         setVisible(true);
+
+        // title set
+        title.setText(_title);
+        title.setHorizontalAlignment(JLabel.CENTER);
+        title.setLocation(title_position_x,title_position_y);
+        title.setSize(title_size_width,title_size_height);
+        title.setOpaque(true);
+        title.setBackground(Color.WHITE);
+        add(title);
+
+        //extras set
+        extras.setSize(bottomGroup_size_width,bottomGroup_size_height);
+        extras.setHorizontalAlignment(JLabel.CENTER);
+        extras.setText("+"+Integer.toString(3));
+        extras.setLocation(extras_position_x, extras_position_y);
+        add(extras);
+        extras.setOpaque(true);
+        setBackground(Color.WHITE);
+        
+        
+        
+    }
+    
+    public void paintComponent(Graphics g) {
+        super.paintComponents(g);
+        // bored set
+        g.setColor(bored_backColor);
+        g.fillRect(0, 0, bored_size_width, bored_size_height);
+        // image set
+        g.drawImage(image.getImage(), image_position_x, image_position_y, image_size_width, image_size_height, this);
+        // host set
+
+        // guset set
+
+
+    }
+    
+
+    private class Bored_Detail extends JFrame{
+        public Bored_Detail(){
+            
+        }
     }
 }
--- I_dont_want_eat_lonely/src/client_window.java
+++ I_dont_want_eat_lonely/src/client_window.java
@@ -3,13 +3,35 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class client_window extends JFrame{
+    static public void main(String[] args){
+        new client_window("AAAA");
+    }
     public client_window(String titleName){
+        // window setting
         super(titleName);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Container c = getContentPane();
         c.setLayout(new FlowLayout(FlowLayout.LEFT,30,20));
-        c.add(new Bored());
-        c.add(new Bored());
         setSize(500,550);
+        
+        // compoenet add
+        c.add(new Bored());
+        c.add(new MenuPanel());
+        c.add(new JLabel("CCCC"));
+
+        // show component
         setVisible(true);
     }
+
+
+    class MenuPanel extends JPanel{
+		private JButton[] gbbBtn = new JButton[3];
+		public MenuPanel() {
+			setBackground(Color.gray);
+			for(int i=0;i<gbbBtn.length;++i){
+				gbbBtn[i]=new JButton("AAAA");
+				add(gbbBtn[i]);
+			}
+		}
+	}
 }
